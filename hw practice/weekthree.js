f454e4306ea62a2d8c92dd05109f3e3b3c79d505
savstar14 <84650269+savstar14@users.noreply.github.com>
2021-06-10 22:02:53 -0700

    renamed html

--- hw practice/weekthree.js
+++ hw practice/weekthree.js
@@ -2,14 +2,38 @@
 //1. 1.	Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
 //a.	Programmatically subtract the value of the first element in the array
 // from the value in the last element of the array (do not use numbers to reference the last element, 
+
+var ages = [3, 9, 23, 64, 2, 8, 28, 93];
+
 //find it programmatically, ages[7] – ages[0] is not allowed). Print the result to the console.
 //b.	Add a new age to your array and repeat the step above to ensure 
 //it is dynamic (works for arrays of different lengths).
 //c.	Use a loop to iterate through the array and calculate the average age.
 // Print the result to the console.
+{
+    var ages = [3, 9, 23, 64, 2, 8, 28, 93];
+    ages.length = 15;
+    ages[16] = 40;
+    
+    let count = 0;
+    let total = 0;
 
+    for(let i = 0; i < ages.length; i++){
+        if(ages[i] != undefined){
 
+            count++;
+            total += ages[i];
+        }
+    }
+
+    let avg = total / count;
+    console.log(avg);
+}
 //2.	Create an array called names that contains the following values: ‘Sam’, ‘Tommy’, ‘Tim’, ‘Sally’, ‘Buck’, ‘Bob’.
+{
+    var names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+}
+
 //a.	Use a loop to iterate through the array and calculate the average number of letters per name.
 // Print the result to the console.
 //b.	Use a loop to iterate through the array again and concatenate all the names together, 
@@ -17,21 +41,35 @@
 
 
 //3.	How do you access the last element of any array?
+//The last elemeent of the array is retrieved is shown as:
+var myArray = [1, 2, 3];
+var lastElement = myArray[myArray.length - 1];
+console.log(lastElement);
+
+
 
 
 //4.	How do you access the first element of any array?
-
+//The first element in an array is retrieved by asking for the 0 index of the array.
+//example:
+var myArray = [1, 2, 3];
+myArray[0] = 1;
+console.log(myArray[0]);
 
 //5.	Create a new array called nameLengths.
 // Write a loop to iterate over the previously created
 // names array and add the length of each name to the nameLengths array.
 
 
-//6.	Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array. Print the result to the console.
+//6.	Write a loop to iterate over the nameLengths array 
+//and calculate the sum of all the elements in the array. 
+//Print the result to the console.
 
 
 //7.	Write a function that takes two parameters, 
-//word and n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in ‘Hello’ and 3, I would expect the function to return ‘HelloHelloHello’).
+//word and n, as arguments and returns the word concatenated to itself 
+//n number of times. (i.e. if I pass in ‘Hello’ and 3, 
+//I would expect the function to return ‘HelloHelloHello’).
 
 
 //8.	Write a function that takes two parameters, 
